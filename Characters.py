aedf382fa034b1445ab047d06188cfa6cd6b5a40
theselfme <wasne120@gmail.com>
2026-08-05 09:48:44 +0200

    derived class of types of characters in superclass of general characters

--- Characters.py
+++ Characters.py
@@ -15,21 +15,75 @@ class Character:
         )
 
 
-    def update(self):
+    def update_rect(self):
         self.rect.center = self.pos
 
 
     def draw(self, surface):
+
         if self.image:
-            surface.blit(
-                self.image,
-                self.rect
-            )
+            surface.blit(self.image, self.rect)
+
         else:
-            # placeholder
             pygame.draw.circle(
                 surface,
-                "red",
+                "white",
                 self.pos,
                 self.size
             )
+
+
+
+class Creature(Character):
+
+    def __init__(self, x, y):
+        super().__init__(x, y, 40)
+
+        self.speed = 500
+        self.health = 100
+
+
+    def update(self, dt):
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.pos.x -= self.speed * dt
+
+        if keys[pygame.K_d]:
+            self.pos.x += self.speed * dt
+
+        self.update_rect()
+
+
+
+    def attack(self):
+        print("Atak!")
+
+
+
+class GhostMouse(Character):
+
+    def __init__(self, x, y):
+        super().__init__(x, y, 20)
+
+        self.interaction_range = 100
+
+
+    def update(self):
+
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        self.pos.x = mouse_x
+        self.pos.y = mouse_y
+
+        self.update_rect()
+
+
+
+    def interact(self, objects):
+
+        for obj in objects:
+
+            if self.rect.colliderect(obj.rect):
+                print("Duch oddziałuje z obiektem")
--- main.py
+++ main.py
@@ -3,11 +3,26 @@ from ClickableBox import ClickableBox
 from Characters import Characters
 pygame.init()
 
+creature = Creature(400,300)
+ghost = GhostMouse(600,300)
+
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
 box1 = ClickableBox(100, 100, 200, 200, (255, 0, 0), (255, 100, 100), (200, 0, 0))
 box2 = ClickableBox(400, 400, 200, 200, (0, 255, 0), (100, 255, 100), (0, 200, 0))
+
+creature = Character(
+    500,
+    300,
+    40
+)
+
+mouse = Character(
+    700,
+    300,
+    20
+)
 
 running = True
 while running:
